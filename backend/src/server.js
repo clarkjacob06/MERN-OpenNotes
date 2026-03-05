cbe254676fdd6f88b34da71d6f39715b9374d55c
Clark <clarkjacob627@gmail.com>
2026-03-05 18:27:20 +0800

    remove all progress

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,8 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
 app.use('/api/notes', notesRouter);
+
+
 
 connectDB().then(() => {
 app.listen(PORT, () => {
